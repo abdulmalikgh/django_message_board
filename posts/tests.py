7e8ad4d4c6f609ef95b6cef404d2de807bb033d0
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-01-29 16:34:11 +0000

    update with test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class PostModelTest(TestCase):
+    def setup(self):
+        Post.objects.create(text='This is a testing post')
+    
+    def test_text_content(self):
+        post = Post.objects.get(id=2)
+        text = f'{post.text}'
+        self.assertEqual(text,'This is a testing post')
